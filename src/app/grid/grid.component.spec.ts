68894ead0f2e94a694e6e8567e84eafd68cc4844
Andrei <andrei_caleniuc@live.com>
2020-03-23 20:37:14 +0200

    Add small UTs for the Grid component

--- src/app/grid/grid.component.spec.ts
+++ src/app/grid/grid.component.spec.ts
@@ -1,6 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GridComponent } from './grid.component';
+import { GridComponent, SortDirection } from './grid.component';
+
+const testData = [
+  {
+    country: "Germany",
+    code: "DE"
+  },
+  {
+    country: "Italy",
+    code: "IT"
+  },
+  {
+    country: "Romania",
+    code: "RO'"
+  }
+]
 
 describe('GridComponent', () => {
   let component: GridComponent;
@@ -16,10 +31,70 @@ describe('GridComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GridComponent);
     component = fixture.componentInstance;
+    component.data = testData;
     fixture.detectChanges();
+    component.data = [...testData];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a table', () => {
+    const wrapper = fixture.nativeElement;
+    const table = wrapper.querySelector('table');
+    expect(table).toBeTruthy();
+  });
+
+  it('should sort data', () => {
+    const sortProperty = 'country';
+    // No sorting active by default
+    expect(component.isSortActive(sortProperty, SortDirection.ASC)).toBeFalse();
+    expect(component.isSortActive(sortProperty, SortDirection.DESC)).toBeFalse();
+    component.onSort(sortProperty);
+    // ASC sort
+    expect(component.isSortActive(sortProperty, SortDirection.ASC)).toBeTrue();
+    expect(component.isSortActive(sortProperty, SortDirection.DESC)).toBeFalse();
+    component.data.forEach((element, i) => {
+      expect(element[sortProperty]).toBe(testData[i][sortProperty]);
+    });
+    component.onSort(sortProperty);
+    // DESC sort
+    expect(component.isSortActive(sortProperty, SortDirection.ASC)).toBeFalse();
+    expect(component.isSortActive(sortProperty, SortDirection.DESC)).toBeTrue();
+    component.data.forEach((element, i) => {
+      expect(element[sortProperty]).toBe(testData[testData.length - i - 1][sortProperty]);
+    });
+  });
+
+  it('should get the data to display', () => {
+    let displayData = component.getDataToDisplay();
+    expect(displayData.length).toBe(testData.length);
+    component.pageSize = 1;
+    // It return the first element
+    displayData = component.getDataToDisplay();
+    expect(displayData.length).toBe(1);
+    expect(displayData[0].country).toBe(testData[0].country);
+    // It return the second element
+    component.pageNumber = 2;
+    displayData = component.getDataToDisplay();
+    expect(displayData.length).toBe(1);
+    expect(displayData[0].country).toBe(testData[1].country);
+  })
+
+  it('should raise the pageSizeChange event', () => {
+    const event = {target: {value: 25}};
+    component.pageNumber = 2;
+    component.pageSizeChange.subscribe(size => {
+      expect(size).toBe(event.target.value)
+      expect(component.pageNumber).toBe(1);
+    });
+    component.onPageSizeChange(event);
+  })
+
+  it('should raise the pageNumberChange event', () => {
+    const pageIndex = 2;
+    component.pageNumberChange.subscribe(page => expect(page).toBe(pageIndex + 1));
+    component.onPageNumberChange(pageIndex);
+  })
 });
--- src/app/pagination/pagination.component.spec.ts
+++ src/app/pagination/pagination.component.spec.ts
@@ -22,4 +22,54 @@ describe('PaginationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should raise the pageNumberChange event', () => {
+    const pageIndex = 2;
+    component.pageNumberChange.subscribe(index => expect(index).toBe(pageIndex));
+    component.onPageNumberChange(pageIndex);
+  })
+
+  it('should go to first page', () => {
+    component.pageNumber = 5;
+    component.pageNumberChange.subscribe(index => expect(index).toBe(0));
+    component.selectFirstPage();
+  })
+
+  it('should go to previous page', () => {
+    const pageNumber = 5;
+    component.pageNumber = pageNumber;
+    component.pageNumberChange.subscribe(index => expect(index).toBe(pageNumber - 2));
+    component.selectPreviousPage();
+  })
+
+  it('should go to next page', () => {
+    const pageNumber = 5;
+    component.data = Array(10);
+    component.pageNumber = pageNumber;
+    component.pageNumberChange.subscribe(index => expect(index).toBe(pageNumber + 1));
+    component.selectNextPage();
+  })
+
+  it('should go to last page', () => {
+    const totalPages = 10;
+    component.data = Array(totalPages);
+    component.pageNumberChange.subscribe(index => expect(index).toBe(totalPages - 1));
+    component.selectPreviousPage();
+  })
+
+  it('should raise the pageSizeChange event', () => {
+    const event = {target: {value: 25}};
+    component.pageSizeChange.subscribe(e => {
+      expect(e.target.value).toBe(event.target.value)
+    });
+    component.onPageSizeChange(event);
+  })
+
+  it('should raise the pageNumberChange event', () => {
+    const page = 2;
+    component.pageNumberChange.subscribe(p => {
+      expect(p).toBe(page)
+    });
+    component.onPageNumberChange(page);
+  })
 });
